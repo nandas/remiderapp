8bff69d71394ef22f0e0e9d8b882a0a0266e540d
nandas <nandakumararaja@gmail.com>
2018-12-11 23:45:08 -0500

    initial index.js file

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+const App = () => {
+        return (
+          <diV>
+              This is reminder App
+          </diV>
+        );
+};
+
+ReactDOM.render(<App/>,document.querySelector('#root'));
+
+
